85bb9bf2f9a11e16c97faa18097f9b5cc7be557d
GeYuan <120463275@qq.com>
2024-02-16 12:15:36 +0800

    feat: build a style link component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,7 +9,7 @@ import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
-import ErrorMessage from "@/app/components/errorMessage";
+import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
