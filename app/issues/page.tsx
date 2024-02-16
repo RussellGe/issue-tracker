e37a17d7e0ee323de89e405ea982b6e0126f7a8c
GeYuan <120463275@qq.com>
2024-02-16 12:24:53 +0800

    feat: add additional loading pages

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,13 +2,11 @@ import { Table } from "@radix-ui/themes";
 import React from "react";
 import { prisma } from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
 import IssueActions from "./IssueActions";
 import Link from "../components/Link";
 
 const page = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
   return (
     <div>
       <IssueActions />
