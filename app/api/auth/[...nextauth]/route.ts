4c20e418cec00e4612057dbeee2702d64d45ff79
GeYuan <120463275@qq.com>
2024-02-16 17:13:34 +0800

    feat: config google provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,12 @@
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 const handle = NextAuth({
-    providers: []
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        })
+    ]
 })
 export {
     handle as GET,
