76f70c7ee8ee279443c4a63c29d346d3112ee5d9
GeYuan <120463275@qq.com>
2024-02-16 17:28:59 +0800

    feat: add the login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,13 +4,15 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import classNames from "classnames";
-
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 const NavBar = () => {
   const currentPath = usePathname();
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+  const { status, data } = useSession();
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
@@ -32,6 +34,14 @@ const NavBar = () => {
           </li>
         ))}
       </ul>
+      <Box>
+        {status === "authenticated" && (
+          <Link href="/api/auth/signout">Sign Out</Link>
+        )}
+        {status === "unauthenticated" && (
+          <Link href="/api/auth/signin">Sign In</Link>
+        )}
+      </Box>
     </nav>
   );
 };
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,19 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/prisma/client";
+
 const handle = NextAuth({
+    adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
-    ]
+    ],
+    session: {
+        strategy: "jwt",
+    },
 })
 export {
     handle as GET,
