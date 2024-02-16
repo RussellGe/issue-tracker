b3802d35815f171bb8ba5dc80f863ca771e451ad
GeYuan <120463275@qq.com>
2024-02-16 16:08:40 +0800

    feat: improve the loading experience

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,14 +1,1 @@
-import { Skeleton } from "@/app/components";
-import { Box } from "@radix-ui/themes";
-import React from "react";
-
-const loading = () => {
-  return (
-    <Box className="max-w-xl">
-      <Skeleton />
-      <Skeleton height={"20rem"} />
-    </Box>
-  );
-};
-
-export default loading;
+export { default } from "../_components/IssueFormSkeleton";
